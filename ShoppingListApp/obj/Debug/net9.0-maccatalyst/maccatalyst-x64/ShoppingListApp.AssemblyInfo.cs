57091019a582a7aa59e3f70cb4e9b6a2a3499af8
JulOo <julianwidlak@gmail.com>
2026-01-22 11:57:32 +0100

    small change

--- ShoppingListApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/ShoppingListApp.AssemblyInfo.cs
+++ ShoppingListApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/ShoppingListApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3028a87535a138ccff0910f3f5968c8ac237fbbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
